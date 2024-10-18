a28b555d8b047f8bbae03004260671b8c2fb70a8
Dryzera <endryusdaniel6321@gmail.com>
2024-10-18 18:54:31 -0300

    initial commit

---
+++ static/js/script.js
@@ -0,0 +1,40 @@
+const body = document.body
+
+function generateNumbers() {
+    let leftNumber = Math.floor(Math.random() * 11); 
+    let rightNumber = Math.floor(Math.random() * 11);
+
+    let resultMultiplicate = leftNumber * rightNumber;
+
+    return [leftNumber, rightNumber, resultMultiplicate];
+}
+
+function sendToHTML(left, right) {
+    document.getElementById('left-number').textContent = left;
+    document.getElementById('right-number').textContent = right;
+}
+
+function checkToAnswer(answer, resultMultiplicate) {
+    if (answer != resultMultiplicate) {
+        body.style.backgroundColor = '#f53131' // se incorreto
+    } else {
+        body.style.backgroundColor = '#72cb26' // se correto
+    }
+
+    setTimeout(() => {
+        location.reload();
+    }, 800);
+}
+
+let [leftNumber, rightNumber, resultMultiplicate] = generateNumbers();
+
+sendToHTML(leftNumber, rightNumber);
+
+document.getElementById('submit-btn').addEventListener('click', function() {
+    let answer = parseInt(document.getElementById('answer-input').value);
+    checkToAnswer(answer, resultMultiplicate);
+});
+
+document.getElementById('skip-btn').addEventListener('click', function() {
+    location.reload()
+})
